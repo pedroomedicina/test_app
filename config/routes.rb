c839768c190d51cf6161d259c1eca0bc71700665
pedroomedicina <pedromedina190@gmail.com>
2019-08-08 23:16:08 -0400

    adding user edit layout and controller

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,9 @@ Rails.application.routes.draw do
 
   root 'welcome#home'
   get 'about', to: 'welcome#about'
+  get 'signup', to: 'users#new'
 
+  resources :users, except: [:new]
   resources :articles
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
