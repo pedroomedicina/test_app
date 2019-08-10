16109d4c1025109e1b3b75ad9cab4cfc00f5248b
pedroomedicina <pedromedina190@gmail.com>
2019-08-10 19:48:18 -0400

    Restrict Actions in Controller

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,10 @@ Rails.application.routes.draw do
   get 'about', to: 'welcome#about'
   get 'signup', to: 'users#new'
 
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
+  
   resources :users, except: [:new]
   resources :articles
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
