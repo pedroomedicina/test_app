1707419a55ad89087f29c7c65983bec52f0a73fc
pedroomedicina <pedromedina190@gmail.com>
2019-08-13 23:54:21 -0400

    lots of changes

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,30 +1,56 @@
 class CategoriesController < ApplicationController
+  before_action :require_admin, except: [:index, :show]
 
-def index
-  @categories = Category.paginate(page: params[:page], per_page: 5)
-end
-
-def new
-  @category = Category.new
-end
-
-def create
-  @category = Category.new(category_params)
-  if @category.save
-    flash[:success] = "Category created successfully"
-    redirect_to categories_path
-  else
-    render 'new'
+  def index
+    @categories = Category.paginate(page: params[:page], per_page: 5)
   end
-end
 
-def show
-end
+  def new
+    @category = Category.new
+  end
 
-private
+  def create
+    @category = Category.new(category_params)
+    if @category.save
+      flash[:success] = "Category created successfully"
+      redirect_to categories_path
+    else
+      render 'new'
+    end
+  end
 
-def category_params
-  params.require(:category).permit(:name)
-end
+  def edit
+    @category = Category.find(params[:id])
+  end
+
+  def show
+    @category = Category.find(params[:id])
+    @category_articles = @category.articles.paginate(page: params[:page], per_page: 5)
+  end
+
+  def update
+    @category = Category.find(params[:id])
+    if @category.update(category_params)
+      flash[:success] = "Category name was successfully updated"
+      redirect_to category_path(@category)
+    else
+      render 'edit'
+    end
+  end
+
+  private
+
+  def category_params
+    params.require(:category).permit(:name, category_ids: [])
+  end
+
+  def require_admin
+    if !logged_in? || (logged_in? and !current_user.admin?)
+      flash[:danger] = "Only admins can perform those actions"
+      redirect_to categories_path
+    else
+
+    end
+  end
 
 end
